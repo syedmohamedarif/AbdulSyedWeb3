9cec2374ad880eb2d0b8ce8f311193857e973fa9
syedmohamedarif <syedmohamedarif2208@gmail.com>
2025-11-21 00:32:42 +0530

    Revert "formatting"
    This reverts commit c9d63792d46597ecfee77514d04a30ff0e0c293f.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,8 +21,8 @@ export default function Hero() {
       <div className="relative container mx-auto px-4 sm:px-6 lg:px-8 h-full flex items-center">
         <div className="max-w-2xl text-white">
           <h1 className="text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 md:mb-8">
-          Mr Abdul Syed (MBBS, MRCSEd, FRCS)
-            <p className="text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl mt-4 sm:mt-6">Consultant Breast Surgeon in Chelmsford, Essex, UK</p>
+            Consultant Breast Surgeon in Chelmsford, Essex, UK
+            <p className="text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl mt-2">Mr Abdul Syed (MBBS, MRCSEd, FRCS)</p>
           </h1>
           <p className="text-base sm:text-lg md:text-xl mb-6 md:mb-8">
             Leading oncoplastic breast surgeon serving in Essex, UK with
